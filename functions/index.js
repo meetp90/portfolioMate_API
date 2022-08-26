8f9bbfd3300f7098de54a34a7457726ff256b5e8
meetp90 <meetpithadia14@gmail.com>
2022-08-26 18:50:32 +0530

    getEngagement

--- functions/index.js
+++ functions/index.js
@@ -215,26 +215,136 @@ exports.createStakeHolder = functions.https.onRequest((request, response) => {
               });
       });
 
+    //   var object={
+    //     type="",
+    //     id="",
+    //     enagement_between=[],
+    //     enegements_of=""
+    // }
       exports.getEngagement = functions.https
       .onRequest((request, response) => {
           response.set("Access-Control-Allow-Origin", "*");
           response.set("Access-Control-Allow-Headers", "Content-Type");
-          const stakeHolder = request.body;
-          admin
+          var toBeReturned = [];
+          const engagement = request.body;
+          if(engagement.id){
+            admin
               .firestore()
               .collection("Engagement")
+              .where("id", "==", engagement.id)
               .get()
               .then((querySnapshot) => {
-                const orders = [];
                 querySnapshot.forEach((doc) => {
                   const order = doc.data();
-                  orders.push(order);
+                  toBeReturned.push(order);
                 });
-                response.json(orders);
+                response.json(toBeReturned);
               }) 
               .catch((error) => {
                 response.status(500).json({
                   error: error.code,
                 });
               });
+          }
+          else if(engagement.type){
+            admin
+              .firestore()
+              .collection("Engagement")
+              .where("type", "==", engagement.type)
+              .get()
+              .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                  const order = doc.data();
+                  toBeReturned.push(order);
+                });
+                response.json(toBeReturned);
+              })
+              .catch((error) => {
+                response.status(500).json({
+                  error: error.code,
+                });
+              });
+          }
+        else if(engagement.engagement_with.length == 2){
+          admin
+              .firestore()
+              .collection("Engagement")
+              .where("engagement_with", "array-contains", engagement.engagement_with[0])
+              .where("engagement_with", "array-contains", engagement.engagement_with[1])
+              .get()
+              .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                  const order = doc.data();
+                  toBeReturned.push(order);
+                });
+                response.json(toBeReturned);
+              }) 
+              .catch((error) => {
+                response.status(500).json({
+                  error: error.code,
+                });
+              });
+        }
+        else if(engagement.engagement_with.length == 3){
+          admin
+              .firestore()
+              .collection("Engagement")
+              .where("engagement_with", "array-contains", engagement.engagement_with[0])
+              .where("engagement_with", "array-contains", engagement.engagement_with[1])
+              .where("engagement_with", "array-contains", engagement.engagement_with[2])
+              .get()
+              .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                  const order = doc.data();
+                  toBeReturned.push(order);
+                });
+                response.json(toBeReturned);
+              }) 
+              .catch((error) => {
+                response.status(500).json({
+                  error: error.code,
+                });
+              });
+        }
+        else if(engagement.engagement_with.length == 4){
+          admin
+              .firestore()
+              .collection("Engagement")
+              .where("engagement_with", "array-contains", engagement.engagement_with[0])
+              .where("engagement_with", "array-contains", engagement.engagement_with[1])
+              .where("engagement_with", "array-contains", engagement.engagement_with[2])
+              .where("engagement_with", "array-contains", engagement.engagement_with[3])
+              .get()
+              .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                  const order = doc.data();
+                  toBeReturned.push(order);
+                });
+                response.json(toBeReturned);
+              }) 
+              .catch((error) => {
+                response.status(500).json({
+                  error: error.code,
+                });
+              });
+        }
+        else if(engagement.engagement_of){
+          admin
+              .firestore()
+              .collection("Engagement")
+              .where("engagement_with", "array-contains", engagement.engagement_of)
+              .get()
+              .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                  const order = doc.data();
+                  toBeReturned.push(order);
+                });
+                response.json(toBeReturned);
+              }) 
+              .catch((error) => {
+                response.status(500).json({
+                  error: error.code,
+                });
+              });
+        }
       });
